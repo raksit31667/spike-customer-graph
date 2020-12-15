e976f503e53bf9f8716c27840750e013b098b1c4
Raksit <raksit.m@ku.th>
2020-12-15 10:38:17 +0700

    :tada:

---
+++ src/main/java/com/raksit/spike/customer/SpikeCustomerGraphApplication.java
@@ -0,0 +1,13 @@
+package com.raksit.spike.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeCustomerGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpikeCustomerGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raksit/spike/customer/SpikeCustomerGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raksit.spike.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikeCustomerGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
